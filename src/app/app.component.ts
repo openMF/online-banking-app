f0b464b2d56b79140acc59ce9f78394b02cd2873
Unknown <rigrod1999@gmail.com>
2019-08-02 01:46:47 +0530

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,109 +25,15 @@ export class AppComponent implements OnInit, OnDestroy {
   private readonly mobileQueryListener: () => void;
   isLoggedIn = false;
   sidenavContents: any = [
-    // {
-    //   title: 'DASHBOARD',
-    //   icon: 'home',
-    //   color: 'pink',
-    //   link: 'dashboard'
-    // },
-    // {
-    //   title: 'ACCOUNTS',
-    //   icon: 'account_balance',
-    //   link: 'accounts'
-    // },
-    // {
-    //   title: 'RECENT TRANSACTIONS',
-    //   icon: 'monetization_on',
-    //   link: 'recent-transactions'
-    // },
-    // {
-    //   title: 'CHARGES',
-    //   icon: 'monetization_on',
-    //   link: 'charges'
-    // },
-    // {
-    //   title: 'TRANSFERS',
-    //   icon: 'monetization_on',
-    //   link: 'transfers'
-    // },
-    // {
-    //   title: 'THIRD PARTY TRANSFERS',
-    //   icon: 'monetization_on',
-    //   link: 'third-party-transfers'
-    // },
-    // {
-    //   title: 'BENEFICIARIES',
-    //   icon: 'supervised_user_circle',
-    //   link: 'beneficiaries'
-    // },
-    // {
-    //   title: 'REPORTS',
-    //   icon: 'assessment',
-    //   link: 'reports'
-    // },
-    // {
-    //   title: 'APPLY FOR LOAN',
-    //   icon: 'assignment_ind',
-    //   link: 'apply/loan'
-    // },
-    // {
-    //   title: 'APPLY FOR SAVINGS',
-    //   icon: 'assignment_ind',
-    //   link: 'apply/savings'
-    // },
-    // {
-    //   title: 'APPLY FOR SHARES',
-    //   icon: 'assignment_ind',
-    //   link: 'apply/shares'
-    // },
-    // {
-    //   title: 'PROFILE',
-    //   icon: 'person',
-    //   link: 'profile'
-    // },
-    // {
-    //   title: 'SURVEY',
-    //   icon: 'format_list_bulleted',
-    //   link: 'surveys'
-    // },
-    // {
-    //   title: 'ABOUT US',
-    //   icon: 'info',
-    //   link: 'about-us'
-    // },
-    // {
-    //   title: 'HELP',
-    //   icon: 'help_outline',
-    //   link: 'help'
-    // },
-    // {
-    //   title: 'login',
-    //   icon: 'person',
-    //   link: 'login'
-    // },
-    // {
-    //   title: 'Notifications',
-    //   icon: 'notifications',
-    //   link: 'notifications'
-    // },
-    // {
-    //   title: 'Pocket',
-    //   icon: 'work',
-    //   link: 'pocket'
-    // }
-
-
+    {
+      title: 'login',
+      icon: 'person',
+      link: 'login'
+    },
   ];
   title = 'Online-Banking-App-3.0';
   mobileQuery: MediaQueryList;
   ngOnInit() {}
-  onNotificationComponent() {
-    this.router.navigate(['notifications']);
-  }
-  onProfileComponent() {
-    this.router.navigate(['profile']);
-  }
   ngOnDestroy(): void {
     this.mobileQuery.removeListener(this.mobileQueryListener);
   }
