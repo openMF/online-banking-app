08c19a03644d92bc6aa58f7db9a84cba85e079d0
Unknown <rigrod1999@gmail.com>
2019-08-09 18:36:01 +0530

    add charges component
    add pagination and searching

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './authentication/login/login.component';
+import { ChargesComponent } from './charges/charges.component';
 const routes: Routes = [
   {path: '', component : LoginComponent},
+  {path: 'charges', component : ChargesComponent},
   {path: '**', redirectTo : ''}
 ];
 @NgModule({
---
+++ src/app/services/charges.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Charges } from '../models/Charges';
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json',
+'Fineract-Platform-TenantId': 'default',
+Authorization: 'Basic bWlmb3M6cGFzc3dvcmQ=' })
+};
+@Injectable({
+  providedIn: 'root'
+})
+export class ChargesService {
+
+  constructor(private http: HttpClient) { }
+
+  getCharges(): Observable<any[]> {
+    return this.http.get<any[]>
+    ('https://demo.openmf.org/fineract-provider/api/v1/clients/1/charges?username=mifos&password=password', httpOptions);
+  }
+}
